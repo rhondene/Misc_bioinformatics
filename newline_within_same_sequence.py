06ac67dd2ad209158ce2285ee69d39de0cab0504
rhondene_wint <rhondenewint93@gmail.com>
2019-07-18 14:07:24 -0700

    Add files via upload
    How to parse a fasta file when  a newline separates nucleotides of the same sequence or entry.

---
+++ newline_within_same_sequence.py
@@ -0,0 +1,31 @@
+##An example when a newline separates nucleotides of the same sequence
+fasta = []
+file=[]
+with open('host_transc_introns.fasta', 'r') as f:
+    for line in f:
+        file.append(line.strip())
+            
+    for i in range(len(file)):
+        line = file[i]
+        if line.startswith('>') is True:
+            header =(line.rstrip())
+            seq = ""
+            j=i  #the index for the sequence
+            while True:
+                j+=1  #the sequence is one position right of the header line
+                if j==len(file):
+                    break
+                if file[j].startswith('>') is True:
+                    break  #when it encounters another header
+                else:
+                    s = file[j]
+                    seq+=s
+            fasta.append(header)
+            fasta.append(seq)
+        else:
+            continue
+			
+### re-rewrite the file as a proper fasta such that only newline between sequences and headers
+with open('host_transc_introns_V2.fasta', 'w') as f:
+    for line in fasta:
+        f.write(line+'\n')
